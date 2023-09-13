b59e286a322c8b65fa14087b89044f6fce36d9f2
Reza Soltani <soltanireza65@gmail.com>
2023-09-13 21:07:13 +0000

    renew token

--- src/constants.ts
+++ src/constants.ts
@@ -4,5 +4,5 @@ export const BOLT_PORT = process.env.BOLT_PORT || 12000
 
 export const boltConfig = {
     signingSecret: "aced12b5ca8cb59e41c49d08cfc16739",
-    token: "xoxb-3479152574481-5894728209572-rOx8I10ILrM6jVm4NKGQ65B8",
+    token: "xoxb-3479152574481-5894728209572-TlITrwkocZLEhehxZvKVGDQ2",
 }

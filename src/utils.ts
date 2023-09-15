0613c1734d1fc82c306800cef2f5f0d696fe3961
Reza Soltani <soltanireza65@gmail.com>
2023-09-15 13:04:18 +0000

    refactor

--- src/utils.ts
+++ src/utils.ts
@@ -3,13 +3,16 @@ import {
     addStringNoLocale,
     createThing,
     getSolidDataset,
+    addNamedNode,
     saveSolidDatasetAt,
+
     setThing
 } from "@inrupt/solid-client";
 
 export const createChat = async (content: string) => {
     const indexUrl = "https://michielbdejong.solidcommunity.net/shops/Chat/id1694605963871/index.ttl"
     const dataset = await getSolidDataset(indexUrl);
+
     let chat
     chat = addDatetime(createThing(), "http://purl.org/dc/terms/created", new Date());
     chat = addStringNoLocale(chat, "http://rdfs.org/sioc/ns#content", content);
@@ -18,5 +21,6 @@ export const createChat = async (content: string) => {
     const updatedThing = setThing(dataset, chat);
 
     const updatedDataset = await saveSolidDatasetAt(indexUrl, updatedThing);
+
     console.log("updatedDataset", updatedDataset);
 };

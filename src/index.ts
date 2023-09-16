e5691998b6f8896aa0d5f8283e70fc6e45da0079
Reza Soltani <soltanireza65@gmail.com>
2023-09-16 15:21:11 +0330

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { App, LogLevel } from "@slack/bolt";
 import { PORT } from "./config/default";
 import { expressReceiver } from "./expressReceiver";
 import { IMessage } from "./types";
-import { createChat } from "./utils";
+import { createChat, createChatHttp } from "./utils";
 import { getSessionFromStorage } from "@inrupt/solid-client-authn-node";
 import axios from "axios";
 import { ParamsIncomingMessage } from "@slack/bolt/dist/receivers/ParamsIncomingMessage";
@@ -30,29 +30,9 @@ const app = new App({
 
 app.message(async ({ message, say, context }) => {
   const _message = message as IMessage;
-  // fetch('https://jsonplaceholder.typicode.com/todos/1')
-  //   .then(response => response.json())
-  //   .then(json => console.log(json))
-  // if (_message.text) {
-  //   var myHeaders = new Headers();
-  //   myHeaders.append("Content-Type", "application/json");
-
-  //   const raw = JSON.stringify({
-  //     "text": "sldkjnlkj"
-  //   });
-
-  //   fetch("https://8000-pondersourc-solidslackb-g76h1bqdgog.ws-eu104.gitpod.io/write-to-pod", {
-  //     method: 'POST',
-  //     headers: myHeaders,
-  //     body: raw,
-  //     redirect: 'follow'
-  //   })
-  //     .then(response => response.text())
-  //     .then(result => console.log(result))
-  //     .catch(error => console.log('error', error));
-  // }
-
-
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => console.log(json))
 
   // console.log("context", context);
   // console.log(`context.req.webId`, context.req.webId);
@@ -61,9 +41,6 @@ app.message(async ({ message, say, context }) => {
   // console.log(`session`, session);
   // console.log(`Logged in with the WebID ${session?.info.webId}`);
 
-  let data = JSON.stringify({
-    "text": "kjnkljn"
-  });
 
   let config = {
     method: 'post',
@@ -72,7 +49,9 @@ app.message(async ({ message, say, context }) => {
     headers: {
       'Content-Type': 'application/json'
     },
-    data: data
+    data: JSON.stringify({
+      "text": _message.text
+    })
   };
 
   axios.request(config)
@@ -90,19 +69,7 @@ app.message(async ({ message, say, context }) => {
 
 
     // await createChat(_message.text);
-    // let data = `INSERT DATA { <https://michielbdejong.solidcommunity.net/shops/Chat/id1694605963871/index.ttl#this> <http://www.w3.org/2005/01/wf/flow#message> <https://michielbdejong.solidcommunity.net/shops/Chat/id1694605963871/index.ttl#Msg1694606282236> .\n<https://michielbdejong.solidcommunity.net/shops/Chat/id1694605963871/index.ttl#Msg1694606282236> <http://rdfs.org/sioc/ns#content> "${_message.text}\\n" .\n<https://michielbdejong.solidcommunity.net/shops/Chat/id1694605963871/index.ttl#Msg1694606282236> <http://purl.org/dc/terms/created> "2023-09-13T11:58:02Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .\n<https://michielbdejong.solidcommunity.net/shops/Chat/id1694605963871/index.ttl#Msg1694606282236> <http://xmlns.com/foaf/0.1/maker> <https://solid-crud-tests-example-1.solidcommunity.net/profile/card#me> .\n }\n`;
-    // let resp = await axios.patch(
-    //   `https://michielbdejong.solidcommunity.net/shops/Chat/id1694605963871/index.ttl`,
-    //   data,
-    //   {
-    //     headers: {
-    //       "content-type": "application/sparql-update",
-    //     },
-    //   }
-    // );
-    // console.log("...............");
-    // console.log(resp.data);
-    // console.log("...............");
+    // await createChatHttp(_message);
   } catch (error: any) {
     console.log(error.message);
   }
@@ -112,20 +79,3 @@ app.message(async ({ message, say, context }) => {
   await app.start(PORT);
   console.log("⚡️ Bolt app started");
 })();
-
-
-function getBody(request: ParamsIncomingMessage) {
-  return new Promise((resolve) => {
-    const bodyParts: any[] = [];
-    let body = "";
-    request.on('readable', function () {
-      body += request.read();
-    });
-    request.on('data', (chunk) => {
-      bodyParts.push(chunk);
-    }).on('end', () => {
-      body = Buffer.concat(bodyParts).toString();
-      resolve(body)
-    });
-  });
-}
--- src/utils.ts
+++ src/utils.ts
@@ -8,6 +8,9 @@ import {
 
     setThing
 } from "@inrupt/solid-client";
+import { ParamsIncomingMessage } from "@slack/bolt/dist/receivers/ParamsIncomingMessage";
+import axios from "axios";
+import { IMessage } from "./types";
 
 export const createChat = async (content: string) => {
     const indexUrl = "https://michielbdejong.solidcommunity.net/shops/Chat/id1694605963871/index.ttl"
@@ -24,3 +27,37 @@ export const createChat = async (content: string) => {
 
     console.log("updatedDataset", updatedDataset);
 };
+
+export function getBody(request: ParamsIncomingMessage) {
+    return new Promise((resolve) => {
+        const bodyParts: any[] = [];
+        let body = "";
+        request.on('readable', function () {
+            body += request.read();
+        });
+        request.on('data', (chunk) => {
+            bodyParts.push(chunk);
+        }).on('end', () => {
+            body = Buffer.concat(bodyParts).toString();
+            resolve(body)
+        });
+    });
+}
+
+
+
+export async function createChatHttp(msg: IMessage) {
+    let data = `INSERT DATA { <https://michielbdejong.solidcommunity.net/shops/Chat/id1694605963871/index.ttl#this> <http://www.w3.org/2005/01/wf/flow#message> <https://michielbdejong.solidcommunity.net/shops/Chat/id1694605963871/index.ttl#Msg1694606282236> .\n<https://michielbdejong.solidcommunity.net/shops/Chat/id1694605963871/index.ttl#Msg1694606282236> <http://rdfs.org/sioc/ns#content> "${msg.text}\\n" .\n<https://michielbdejong.solidcommunity.net/shops/Chat/id1694605963871/index.ttl#Msg1694606282236> <http://purl.org/dc/terms/created> "2023-09-13T11:58:02Z"^^<http://www.w3.org/2001/XMLSchema#dateTime> .\n<https://michielbdejong.solidcommunity.net/shops/Chat/id1694605963871/index.ttl#Msg1694606282236> <http://xmlns.com/foaf/0.1/maker> <https://solid-crud-tests-example-1.solidcommunity.net/profile/card#me> .\n }\n`;
+    let resp = await axios.patch(
+        `https://michielbdejong.solidcommunity.net/shops/Chat/id1694605963871/index.ttl`,
+        data,
+        {
+            headers: {
+                "content-type": "application/sparql-update",
+            },
+        }
+    );
+    console.log("...............");
+    console.log(resp.data);
+    console.log("...............");
+}
